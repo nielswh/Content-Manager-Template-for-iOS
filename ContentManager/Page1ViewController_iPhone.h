9595e8ecda0b7264cba0a9cf0635c02ccd38a136
Niels Hansen <nielswhansen@mac.com>
2011-10-10 23:22:56 -0700

    Adding Files

---
+++ ContentManager/Page1ViewController_iPhone.h
@@ -0,0 +1,16 @@
+//
+//  Page1ViewController_iPhone.h
+//  ContentManager
+//
+//  Created by Niels Hansen on 10/10/11.
+//  Copyright 2011 Agilite Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Page1ViewController.h"
+
+@interface Page1ViewController_iPhone : Page1ViewController {
+    
+}
+
+@end
---
+++ ContentManager/Page2ViewController_iPhone.m
@@ -0,0 +1,58 @@
+//
+//  Page2ViewController_iPhone.m
+//  ContentManager
+//
+//  Created by Niels Hansen on 10/10/11.
+//  Copyright 2011 Agilite Software. All rights reserved.
+//
+
+#import "Page2ViewController_iPhone.h"
+
+
+@implementation Page2ViewController_iPhone
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    NSLog(@"Page 2 cleaning up");
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
